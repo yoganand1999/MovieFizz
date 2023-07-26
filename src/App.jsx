4bec8eea0113b50e12f7a5956a54b56d49556445
Yoganand <yoganandmanoj303@gmail.com>
2023-07-26 12:12:50 +0530

    app.jsx was fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { getApiConfiguration, getGeneres } from "./store/homeSlice";
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
-import Details from './pages/details/Details';
-import SearchResult from './pages/searchresult/searchResult';
+import Details from "./pages/details/Details";
+import SearchResult from './pages/searchresult/SearchResult';
 import Explore from './pages/explore/Explore';
 import PageNotFound from './pages/404/PageNotFound';
 
