c85f7164d38ac3b5e2e3f5e10a35120fa5c95a89
Yoganand <yoganandmanoj303@gmail.com>
2023-07-26 12:28:41 +0530

    popular fixed

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import Carousel from '../../../components/carousel/Carousel';
 
-import SwitchTabs from '../../../components/switchtabs/switchTabs';
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
 import useFetch from '../../../hooks/useFetch';
 
